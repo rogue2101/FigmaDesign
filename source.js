013b61063667dd304668631ef11f08e92c3351bd
rogue2101 <rajpriyasingh2002@gmail.com>
2025-01-20 13:17:00 +0530

    first commit

---
+++ source.js
@@ -0,0 +1,25 @@
+sidebar = document.querySelector('.sidebar');
+mainContent = document.querySelector('.content-space ');
+hamburger = document.querySelector('.hamburger')
+addIcon = document.querySelector('.add-button')
+menuButton = document.querySelector('.button-dot')
+menu = document.querySelector('.dot-menu')
+
+sidebarArrow = document.querySelector('.sidebar-arrow');
+
+sidebarArrow.addEventListener('click', (event) => {
+    sidebar.style.display = 'none';
+    mainContent.style.width = '100%';
+    console.log(sidebar)
+});
+
+hamburger.addEventListener('click', (event) => {
+    mainContent.style.width = '80%';
+    sidebar.style.display = 'flex';
+})
+
+menuButton.addEventListener('click', () => {
+    menuButton.style.zIndex = '-1';
+menu.style.display='initial';
+})
+
