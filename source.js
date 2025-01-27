28983772dfc977593b897687bc595988fddca594
rogue2101 <rajpriyasingh2002@gmail.com>
2025-01-27 14:47:35 +0530

    new changes

--- source.js
+++ source.js
@@ -1,25 +1,24 @@
-sidebar = document.querySelector('.sidebar');
-mainContent = document.querySelector('.content-space ');
-hamburger = document.querySelector('.hamburger')
-addIcon = document.querySelector('.add-button')
-menuButton = document.querySelector('.button-dot')
-menu = document.querySelector('.dot-menu')
+sidebar = document.querySelector(".sidebar");
+mainContent = document.querySelector(".content-space ");
+hamburger = document.querySelector(".hamburger");
+addIcon = document.querySelector(".add-button");
+menuButton = document.querySelector(".button-dot");
+menu = document.querySelector(".dot-menu");
 
-sidebarArrow = document.querySelector('.sidebar-arrow');
+sidebarArrow = document.querySelector(".sidebar-arrow");
 
-sidebarArrow.addEventListener('click', (event) => {
-    sidebar.style.display = 'none';
-    mainContent.style.width = '100%';
-    console.log(sidebar)
+sidebarArrow.addEventListener("click", (event) => {
+  sidebar.style.display = "none";
+  mainContent.style.width = "100%";
+  console.log(sidebar);
 });
 
-hamburger.addEventListener('click', (event) => {
-    mainContent.style.width = '80%';
-    sidebar.style.display = 'flex';
-})
+hamburger.addEventListener("click", (event) => {
+  mainContent.style.width = "80%";
+  sidebar.style.display = "flex";
+});
 
-menuButton.addEventListener('click', () => {
-    menuButton.style.zIndex = '-1';
-menu.style.display='initial';
-})
-
+menuButton.addEventListener("click", () => {
+  menuButton.style.zIndex = "-1";
+  menu.style.display = "initial";
+});
